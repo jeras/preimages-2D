9df51feb24b0ea58355340f20b76f192a3a33390
Iztok Jeras <iztok.jeras@redpitaya.com>
2016-08-25 12:38:11 +0200

    removing declartation for non existing function

--- src/ca2d_network.h
+++ src/ca2d_network.h
@@ -37,9 +37,4 @@
 int ca2d_network_print (ca2d_t ca2d, ca2d_size_t siz, int unsigned res [siz.y] [siz.x] [ca2d.ngb.n]);
 int ca2d_network       (ca2d_t ca2d, ca2d_size_t siz, int unsigned ca [siz.y] [siz.x], mpz_t cnt [2], int unsigned list [] [siz.y+ca2d.ver.y] [siz.x+ca2d.ver.x]);
 
-int ca2d_network_preimage (ca2d_t ca2d, ca2d_size_t siz,
-    int unsigned res [siz.y] [siz.x] [(size_t) pow (ca2d.sts, ca2d.ngb.y * ca2d.ngb.x)],
-    int unsigned ca [siz.y+(ca2d.ngb.y-1)] [siz.x+(ca2d.ngb.x-1)]
-);
-
 #endif
